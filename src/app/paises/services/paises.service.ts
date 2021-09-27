439f3341c298b1216022bbe22327fcda57bf0cf4
tonocp <amcp88@gmail.com>
2021-09-27 07:02:22 +0200

    Nested Selectors App v2.0

--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -4,43 +4,47 @@ import { Pais, PaisSmall } from '../interfaces/paises.interface';
 import { combineLatest, Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisesService {
-  
-  private baseUrl: string = 'https://restcountries.eu/rest/v2';
-  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  private baseUrl: string = 'https://restcountries.com/v2';
+  private _regiones: string[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
 
   get regiones(): string[] {
     return [...this._regiones];
   }
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  getPaisesPorRegion( region : string ): Observable<PaisSmall[]> {
-    const url: string = `${this.baseUrl}/region/${region}?fields=alpha3Code;name`;
-    return this.http.get<PaisSmall[]>( url );
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]> {
+    const url: string = `${this.baseUrl}/continent/${region}?fields=alpha3Code,name`;
+    return this.http.get<PaisSmall[]>(url);
   }
 
-  getPaisPorCodigo( codigo : string ): Observable<Pais | null> {
-    if(!codigo) return of(null);
+  getPaisPorCodigo(codigo: string): Observable<Pais | null> {
+    if (!codigo) return of(null);
     const url: string = `${this.baseUrl}/alpha/${codigo}`;
     return this.http.get<Pais>(url);
   }
 
-  getPaisPorCodigoSmall( codigo : string ): Observable<PaisSmall> {
-    const url: string = `${this.baseUrl}/alpha/${codigo}?fields?alpha3Code;name`;
+  getPaisPorCodigoSmall(codigo: string): Observable<PaisSmall> {
+    const url: string = `${this.baseUrl}/alpha/${codigo}?fields?alpha3Code,name`;
     return this.http.get<PaisSmall>(url);
   }
 
-  getPaisesPorCodigos( borders: string[] ): Observable<PaisSmall[]>{
-    if(!borders) return of([]);
+  getPaisesPorCodigos(borders: string[]): Observable<PaisSmall[]> {
+    if (!borders) return of([]);
     const peticiones: Observable<PaisSmall>[] = [];
-    borders.forEach( codigo => {
+    borders.forEach((codigo) => {
       const peticion = this.getPaisPorCodigoSmall(codigo);
       peticiones.push(peticion);
     });
     return combineLatest(peticiones);
   }
-  
 }
